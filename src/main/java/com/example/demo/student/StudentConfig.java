e7038c2aace0744c65d62b91e05497352bdaa453
wiggapony0925 <ninjeff06@gmail.com>
2023-11-04 22:10:23 -0400

    config file to set inital data in db  cereated a service class

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+// static removes the Month.SEPTEMBER into just SEPTEMBER
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+           Student jeffrey = new Student(1L,
+                   "Jeffrey Fernandez",
+                   "ninjeff06@gmail.com",
+                   17,
+                   LocalDate.of(2000, SEPTEMBER, 25),
+                   Student.Gender.MALE);
+           
+           Student ryan = new Student(3L,
+                   "Ryan dope",
+                   "ryan@gmail.com",
+                   12,
+                   LocalDate.of(2010, SEPTEMBER, 24),
+                   Student.Gender.MALE);
+
+           repository.saveAll(List.of(jeffrey,ryan));
+        };
+    }
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,19 +1,21 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
-@Service   // Service or bean
+@Service
 public class StudentService {
-    @GetMapping
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L, "Jeffrey Fernandez", "ninjeff06@gmail.com", 17, LocalDate.of(2000, Month.SEPTEMBER, 25), Student.Gender.MALE)
-        );
+        return studentRepository.findAll();
     }
 }
