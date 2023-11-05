3b3299d9d01948434b65140dd0a70063bb7be5a8
wiggapony0925 <ninjeff06@gmail.com>
2023-11-04 20:32:58 -0400

    First draft of Api Layer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,33 +1,14 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.lang.reflect.MalformedParametersException;
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-import java.util.Locale;
 
 @SpringBootApplication
-@RestController //return joson
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping
-	public String helloApplication() {
-		return "<h1>Hello Application, backend</h1>";
-	}
 
-	@GetMapping("/student")
-	public List<Student> displayStudents() {
-		return List.of(
-				new Student(1L, "Jeffrey Fernandez", "ninjeff06@gmail.com", 17, LocalDate.of(2000, Month.SEPTEMBER, 25), Student.Gender.MALE)
-		);
-	}
 }
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(1L, "Jeffrey Fernandez", "ninjeff06@gmail.com", 17, LocalDate.of(2000, Month.SEPTEMBER, 25), Student.Gender.MALE)
+        );
+    }
+}
