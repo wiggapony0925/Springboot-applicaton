52a064c33a889dd74d37ce7d7852bdfb4bac317c
wiggapony0925 <ninjeff06@gmail.com>
2023-11-12 13:07:09 -0500

    added ScanBasePackages

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.demo", "com.example.demo.classroom", "com.example.demo.student", "com.example.demo.utils"}) //  used to specify the packages that should be scanned for components
 public class DemoApplication {
 
 	public static void main(String[] args) {
